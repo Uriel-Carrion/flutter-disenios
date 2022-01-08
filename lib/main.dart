720129e17b84392e19c8258dc81936d25a865079
Uriel Carrion <urielcarrion21@gmail.com>
2022-01-08 00:56:57 -0600

    Creación de la primera versión de la pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:disenos/screens/basic_design.dart';
 import 'package:flutter/material.dart';
+import 'package:disenos/screens/basic_design.dart';
+import 'package:disenos/screens/scroll_design.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,9 +12,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'basic_design', //Pantalla de inicio
+      initialRoute: 'scroll_screen', //Pantalla de inicio
       //Definición de las rutas en el archivo principal
-      routes: {'basic_design': (_) => BasicDesignScreen()},
+      routes: {
+        'basic_design': (_) => BasicDesignScreen(),
+        'scroll_screen': (_) => ScrollScreen()
+      },
     );
   }
 }
